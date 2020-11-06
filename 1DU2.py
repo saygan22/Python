5a5010421c8fdc0b84b80411893db764cad2f2d9
Kochubei Serhii <saygan22@gmail.com>
2020-11-06 19:04:02 +0100

    1DU4

---
+++ 1DU2.py
@@ -0,0 +1,176 @@
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
+
+
+for i in range(27):
+    for j in range(1):
+        if (i + j) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+
+
+print()
+
+
+for x in range(27):
+    for t in range(1,2):
+        if (x + t) % 2 == 0:
+            print('\033[' + str(43) + 'm', end="\t")
+        else:
+            print('\033[' + str(44) + 'm', end="\t")
+print()
