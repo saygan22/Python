e4a1904033bfcb3776989b957febd0618832e2c1
saygan22 <saygan22@gmail.com>
2021-03-12 21:25:51 +0100

    Druhy pokus semestralky

--- Semestralka_druhy_pokus.py
+++ Semestralka_druhy_pokus.py
@@ -4,12 +4,11 @@ from PIL import Image
 print(np.sctypeDict) # разновидности елементов массива
 im = Image.open('zhivotnie_grach.gif') # создаем елемент pillow для превращения
 im  = im.convert('1') # делаем черно-белым
-
+print(type(im))
 pixels = np.array(im, dtype='int8')
 weight, height = im.size
 
 np.set_printoptions(threshold=100000000, linewidth=203)
-
 
 print(pixels.reshape(weight, height)) #  вывод массива
 print(weight)
@@ -17,6 +16,7 @@ print(height)
 print(type(pixels)) # тип массива
 print(type(pixels[10, 10])) # тип ячейки массива(пикселя)
 print(pixels.size) # количевство пикселей(елементов массива)
+a = weight%8
 print(weight%8) # остаток от деления на 8
 print(pixels.itemsize) # размер ячейки в битах
 print(pixels.size*pixels.itemsize) # размер в битах
@@ -30,12 +30,44 @@ dict = {'A': '00000', 'B': '00001', 'C': '00010', 'D': '00100',
         'Y': '11101', 'Z': '10111',
         '.': '11010', ',': '01011', '?': '11001', ' ': '10011'}
 text = input("Enter message to encode: ").upper()
+
 def encode(text):
-    list_encoded = []
+    list_encoded = ''
     for letter in text:
         for key in dict:
             if key == letter:
-                list_encoded.append(dict[key])
+                list_encoded = list_encoded + (dict[key])
     return list_encoded
-
 print(encode(text))
+
+last_pixels = np.zeros((height, a), dtype=np.int8) #создаем пустой массив нулей заданой формы
+
+
+# for s in last_pixels:
+#     for f in encode(text):
+#
+#       print(s)
+list_of_encoded = []
+for f in encode(text):
+   list_of_encoded.append(int(f)) # создаем список чисел в виде кода по одной цифре
+
+
+b = np.array(list_of_encoded, dtype=np.int8) # создаем векторный масив из кода цифр
+print(b)
+
+
+b.resize((height, a), refcheck=False) # создаем недостающую полосу изображения, лишнее заполняем нулями
+print(b)
+
+encoded_im = np.hstack([pixels, b]) # соединяем основную и закодированную части
+
+
+print(encoded_im.reshape(weight + a, height))
+print(weight + a, height)
+
+img = Image.fromarray(encoded_im) #  переводим назад в изображение
+img.show()
+
+
+
+
